595cac9fc244b00cb603eda92826560aa8c2ba06
Ken Crucillo <kendrick004@gmail.com>
2017-04-05 19:55:41 +0800

    add gravity to the particle object apply the gravity on update of the particle

--- ep9/main.js
+++ ep9/main.js
@@ -4,12 +4,11 @@ window.onload = function(){
         height = canvas.height = window.innerHeight,
         width = canvas.width = window.innerWidth,
         particles = [],
-        numParticles = 100,
-        gravity = vector.create(0, 0.1);
+        numParticles = 100;
 
     for(var i = 0; i < numParticles; i++){
         particles.push(
-            particle.create(width/2, height/3, Math.random() * 5 + 2, Math.random() * Math.PI * 2)
+            particle.create(width/2, height/3, Math.random() * 5 + 2, Math.random() * Math.PI * 2, 0.1)
         );
     }
 
@@ -19,7 +18,6 @@ window.onload = function(){
         context.clearRect(0, 0, width, height);
         for(var i = 0; i< numParticles; i++){
             var p = particles[i];
-            p.accelerate(gravity);
             p.update();
             context.beginPath();
             context.arc(p.position.getX(), p.position.getY(), 4, 0, Math.PI * 2, false);
