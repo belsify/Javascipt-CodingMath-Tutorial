fec0b034c7c483b690d09a4a498f41ce2678a7b5
Ken Crucillo <kendrick004@gmail.com>
2017-04-05 19:53:24 +0800

    add gravity to the explosion animation

--- ep9/main.js
+++ ep9/main.js
@@ -3,18 +3,28 @@ window.onload = function(){
         context = canvas.getContext("2d"),
         height = canvas.height = window.innerHeight,
         width = canvas.width = window.innerWidth,
-        p = particle.create(100, height, 10, -Math.PI / 2),
-        accel = vector.create(0.1, 0.1);
+        particles = [],
+        numParticles = 100,
+        gravity = vector.create(0, 0.1);
+
+    for(var i = 0; i < numParticles; i++){
+        particles.push(
+            particle.create(width/2, height/3, Math.random() * 5 + 2, Math.random() * Math.PI * 2)
+        );
+    }
 
     update();
 
     function update(){
         context.clearRect(0, 0, width, height);
-        p.accelerate(accel);
-        p.update();
-        context.beginPath();
-        context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
-        context.fill();
+        for(var i = 0; i< numParticles; i++){
+            var p = particles[i];
+            p.accelerate(gravity);
+            p.update();
+            context.beginPath();
+            context.arc(p.position.getX(), p.position.getY(), 4, 0, Math.PI * 2, false);
+            context.fill();
+        }
         requestAnimationFrame(update);
     }
 };
