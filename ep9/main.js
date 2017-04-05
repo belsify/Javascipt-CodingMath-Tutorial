f2a325ea7ebbc79b330a223a6b0bef01e10de34a
Ken Crucillo <kendrick004@gmail.com>
2017-04-05 19:50:29 +0800

    add acceleration in particle test the acceleration to single particle

---
+++ ep9/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        height = canvas.height = window.innerHeight,
+        width = canvas.width = window.innerWidth,
+        p = particle.create(100, height, 10, -Math.PI / 2),
+        accel = vector.create(0.1, 0.1);
+
+    update();
+
+    function update(){
+        context.clearRect(0, 0, width, height);
+        p.accelerate(accel);
+        p.update();
+        context.beginPath();
+        context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
+        context.fill();
+        requestAnimationFrame(update);
+    }
+};
