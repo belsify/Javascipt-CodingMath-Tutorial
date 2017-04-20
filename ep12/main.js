013ba19951bf5c87e9f940aae269a6814548e408
Ken Crucillo <kendrick004@gmail.com>
2017-04-20 12:33:33 +0800

    removal of particles in the animation

--- ep12/main.js
+++ ep12/main.js
@@ -3,35 +3,42 @@ window.onload = function(){
         context = canvas.getContext("2d"),
         height = canvas.height = window.innerHeight,
         width = canvas.width = window.innerWidth,
-        p = particle.create(width/2, height/2, 3, Math.random() * Math.PI * 2);
+        particles = [],
+        numParticles = 100;
 
-    p.radius = 100;
+    for(var i = 0; i < numParticles; i++){
+        var p = particle.create(width/2, height/2, Math.random() * 5 + 2, Math.random() * Math.PI * 2);
+        p.radius = 10;
+        particles.push(p);
+    }
+
     update();
 
     function update(){
         context.clearRect(0,0,width,height);
-
-        p.update();
-
-        context.beginPath();
-        context.fillStyle = "#000000";
-        context.arc(p.position.getX(), p.position.getY(), p.radius, 0, Math.PI * 2, false);
-        context.fill();
-
-        if(p.position.getX() - p.radius > width){
-            p.position.setX(-p.radius);
-        }
-        if(p.position.getX() + p.radius < 0){
-            p.position.setX(width + p.radius);
-        }
-        if(p.position.getY() - p.radius > height){
-            p.position.setY(-p.radius);
-        }
-        if(p.position.getY() + p.radius < 0){
-            p.position.setY(height + p.radius);
+        console.log(particles.length);
+        for(var i = 0; i< particles.length; i+= 1){
+            var p = particles[i];
+            p.update();
+            context.beginPath();
+            context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
+            context.fill();
         }
 
+        removeDeadParticles();
 
         requestAnimationFrame(update);
+    }
+
+    function removeDeadParticles(){
+        for(var i = particles.length - 1; i >= 0; i -= 1){
+            var p = particles[i];
+            if(p.position.getX() - p.radius > width ||
+                p.position.getX() + p.radius < 0 ||
+                p.position.getY() - p.radius > height ||
+                p.position.getY() + p.radius < 0){
+                    particles.splice(i,1);
+                }
+        }
     }
 };
